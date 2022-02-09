d08db46df02303abb9c22f728c7f2b09d4dd2fcb
Pierluigi La Rocca <laroccapierluigi1998@gmail.com>
2022-02-09 17:05:22 +0100

    Esercizio base e esercizio bonus completato

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ const root = new Vue (
     {
         el: "#root",
         data: {
-            message: 'Welcome in Vue.js'
+            message: 'Welcome in Vue.js',
+            classe: 'ms_h1',
+            img:{
+                image: 'img/banner.png',
+                description:'banner vue.js',
+                class: 'ms_img'
+            }
+             
         }
 
     }
